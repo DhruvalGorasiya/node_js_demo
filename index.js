c371d192d931ac50da37001686bbabccf2db3f9c
DhruvalGorasiya <dhruvalgorasiya@gmail.com>
2023-02-26 16:24:35 +0530

    add path module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const User = require("./api/user");
 require("./configs/db_connect");
 const bodyParser = require("body-parser");
-// const path = require('node:path');
+const path = require('node:path');
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
-// app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views")));
 app.listen(3000, (error) => {
   if (error) {
     console.log(`${error}  some error is found`);
